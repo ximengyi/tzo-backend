28ae22d6d450f376e4d618faa3b49ed0a7d048bd
mengyi <1203669692@qq.com>
2025-05-23 00:52:47 +0800

    install admin

---
+++ plugin/admin/config/database.php
@@ -0,0 +1,19 @@
+<?php
+return  [
+    'default' => 'mysql',
+    'connections' => [
+        'mysql' => [
+            'driver'      => 'mysql',
+            'host'        => '192.168.71.97',
+            'port'        => '3306',
+            'database'    => 'tzo_backend',
+            'username'    => 'root',
+            'password'    => 'root',
+            'charset'     => 'utf8mb4',
+            'collation'   => 'utf8mb4_general_ci',
+            'prefix'      => '',
+            'strict'      => true,
+            'engine'      => null,
+        ],
+    ],
+];
